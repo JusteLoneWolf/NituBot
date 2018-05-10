59a9dd2f82df12aa08f7b17395caf7021a1bde13
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-10 22:04:43 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
-const settings = require('../settings.json');
+const settings = require('../config.json');
 exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
