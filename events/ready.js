f2a9f47f0a14e57e0509bd8ab83279701f3ec13b
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-10 22:47:33 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ const settings = require('../config.json');
 
 module.exports = client => { // eslint-disable-line no-unused-vars
   console.log(purplecolor(`${client.user.username} en ligne. \n` + 'Connecté dans:\n' + client.guilds.array()));
- client.user.setActivity(settings.prefix + `help | ${bot.guilds.size} serveurs  | ${bot.users.size} utilisateurs`,{type: "WATCHING"});
+ client.user.setActivity(settings.prefix + `help | ${client.guilds.size} serveurs  | ${client.users.size} utilisateurs`,{type: "WATCHING"});
   //client.user.setAvatar("PdpPourLeBot");
   
 }; 
